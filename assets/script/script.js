ae8400f63e2a83d8fa9573f19f9939e3fc242839
Matt Beachey <mattbeachey@gmail.com>
2019-11-26 01:24:35 -0600

    laid out, connected API to more channels

--- assets/script/script.js
+++ assets/script/script.js
@@ -1,17 +1,31 @@
 
+const searchBtnEl = document.getElementById("searchButton")
+searchBtnEl.addEventListener("click", function(){
+    weatherSearch()
+})
 
 
-const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + ciyName + "&units=imperial&appid=dd1c6f0d66cbae457daf01f8f6dbe7ff";
+function weatherSearch (){
+const cityName = document.getElementById("cityInput").value
+console.log(cityName)
 
-axios.get("https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&units=imperial&appid=dd1c6f0d66cbae457daf01f8f6dbe7ff")
+
+const queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cityName + "&units=imperial&appid=dd1c6f0d66cbae457daf01f8f6dbe7ff";
+
+axios.get(queryURL)
     .then(function (response) {
-        const city = document.getElementsByClassName("city")
-        const wind = document.getElementsByClassName("wind")
+        const cityEl = document.getElementsByClassName("city")
+        const dateEl = document.getElementById("date")
+        const iconEl = document.getElementById("icon")
+        const tempEl = document.getElementById("temp")
+        const windspeedEl = document.getElementById("wind")
         const humidity = document.getElementsByClassName("humity")
-        const temp = document.getElementsByClassName("temp")
+        const uvIndexEl = document.getElementsByClassName("uvIndex")
         console.log(response);
     });
 
+}
 
 // open weather API key: dd1c6f0d66cbae457daf01f8f6dbe7ff
 // other "default" key: 12a00623f47777267b83242eca269eda
+//test full url: "https://api.openweathermap.org/data/2.5/weather?q=Bujumbura,Burundi&units=imperial&appid=dd1c6f0d66cbae457daf01f8f6dbe7ff"
